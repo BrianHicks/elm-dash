07ef3448e32531f6465f1a0398d489bf3a88ee2d
Brian Hicks <brian@brianthicks.com>
2017-10-19 06:43:46 -0700

    add TODOs

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,16 @@
+/*
+
+  Things I gotta do:
+
+  * load package.jsons, and render them with elm-static-html or something
+    similar. Ideally ask Elm to tell me all of the metadata I need, too.
+  * write out the sqlite database with the docs
+  * write out the final HTML and sqlite db to the format dash expects
+
+  Stretch goals for the hack day:
+
+  * build local documentation (allow this to be configured somehow?)
+
+ */
+
 console.log("hello, world");
